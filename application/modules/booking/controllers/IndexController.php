182eab2ed95e2486ac3e7215ae5987f958c65700
samlanh <mok_channy@yahoo.com>
2018-05-18 22:03:39 +0700

    commit module booking

---
+++ application/modules/booking/controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+class Booking_indexController extends Zend_Controller_Action {
+	
+	
+public function init()
+    {    	
+     /* Initialize action controller here */
+    	header('content-type: text/html; charset=utf8');
+	}
+	public function indexAction()
+	{
+		$session_user=new Zend_Session_Namespace('auth_travel');
+		if (empty($session_user->user_id)){
+			$this->_redirect("/");
+		}
+// 		$db = new Home_Model_DbTable_DbDashboard();
+// 		$this->view->totalBooking = $db->getTotalBooking();
+// 		$this->view->totalDriver = $db->getTotalDriver();
+// 		$this->view->totalCustomer = $db->getTotalCustomer();
+		
+	}
+	
+}
+
---
+++ application/modules/booking/models/DbTable/Dbbooking.php
@@ -0,0 +1,19 @@
+<?php
+
+class Booking_Model_DbTable_Dbbooking extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = '';
+	
+	
+// 	function getTotalBooking(){
+// 		$db = $this->getAdapter();
+// 		$sql="SELECT COUNT(b.`id`) AS totalBooking FROM `tbl_bookings` AS b";
+// 		return $db->fetchOne($sql);
+// 	}
+	
+	
+	
+	
+}
+
