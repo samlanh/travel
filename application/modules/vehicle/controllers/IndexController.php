b99f9b2981dfeab88358d86e8a022c85bfa191ba
samlanh <mok_channy@yahoo.com>
2018-05-31 17:59:17 +0700

    seyha

--- application/modules/vehicle/controllers/IndexController.php
+++ application/modules/vehicle/controllers/IndexController.php
@@ -11,9 +11,52 @@ public function init()
 	{
 		
 	}
+	
 	public function addAction()
 	{
+		$db = new Vehicle_Model_DbTable_DbVehiclePrice();
+		
+		if($this->getRequest()->isPost()){
+			$data = $this->getRequest()->getPost();
+			try{
+				$id= $db->insertPrice($data);
+				if(isset($data['save_close'])){
+					Application_Form_FrmMessage::Sucessfull("INSERT_SUCCESS", '/vehicle/index/index');
+				}else{
+					Application_Form_FrmMessage::Sucessfull("INSERT_SUCCESS", '/vehicle/index/add');
+				}
+			}catch (Exception $e){
+				Application_Form_FrmMessage::message("Application Error");
+				Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+			}
+		}
+		$this->view->supplyer = $db->getAllSupplyer();
+		$this->view->vehicle_type = $db->getAllVehicleType();
+		$this->view->location = $db->getAllLocation();
+	}
 	
+	public function editAction()
+	{
+		$id = $this->getRequest()->getParam("id");
+		$db = new Vehicle_Model_DbTable_DbVehiclePrice();
+	
+		if($this->getRequest()->isPost()){
+			$data = $this->getRequest()->getPost();
+			try{
+				$id= $db->insertPrice($data);
+				Application_Form_FrmMessage::Sucessfull("EDIT_SUCCESS", '/vehicle/index/index');
+			}catch (Exception $e){
+				Application_Form_FrmMessage::message("Application Error");
+				Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+			}
+		}
+		
+		$this->view->row = $db->getAllPriceById($id);
+		$this->view->row_detail = $db->getAllPriceDetailById($id);
+		
+		$this->view->supplyer = $db->getAllSupplyer();
+		$this->view->vehicle_type = $db->getAllVehicleType();
+		$this->view->location = $db->getAllLocation();
 	}
 	
 }
---
+++ application/modules/vehicle/models/DbTable/DbVehiclePrice.php
@@ -0,0 +1,82 @@
+<?php
+class Vehicle_Model_DbTable_DbVehiclePrice extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'tp_price';
+    
+    public function getUserId(){
+    	$db = new Application_Model_DbTable_DbGlobal();
+    	return $db->getUserId();
+    }
+    
+    function insertPrice($data){
+    	$db = $this->getAdapter();
+    	$db->beginTransaction();
+    	try{
+    		$_arr=array(
+    				'supplyerId'		=> $data['supplyerId'],
+    				'vehicleType'	 	=> $data['vehicleType'],
+    				'isAvailable'      	=> $data['isAvailable'],
+    				'note'      		=> $data['note'],
+    				'createDate'	 	=> date("Y-m-d H:i:s"),
+    				'modifyDate'	 	=> date("Y-m-d H:i:s"),
+    				'status'         	=> 1,
+    				'userId'         	=> $this->getUserId(),
+    		);
+    		$price_id =$this->insert($_arr);
+    		
+    		if(!empty($data['identity'])){
+    			$iden = explode(",", $data['identity']);
+    			foreach ($iden as $i){
+    				$arra=array(
+    					'price_id'		=> $price_id,
+    					'from_location'	=> $data['from_location_'.$i],
+    					'to_location'	=> $data['to_location_'.$i],
+    					'duration'		=> $data['duration_'.$i],
+    					'cost'			=> $data['cost_'.$i],
+    					'price'			=> $data['price_'.$i],
+    					'discount'		=> $data['discount_'.$i],
+    				);
+    				$this->_name="tp_price_detail";
+    				$this->insert($arra);
+    			}
+    		}
+    		
+    		$db->commit();
+    	}catch(exception $e){
+    		Application_Form_FrmMessage::message("Application Error");
+    		echo $e->getMessage();
+    		$db->rollBack();
+    	}
+    }
+    
+    
+    function getAllPriceById($id){
+    	$db = $this->getAdapter();
+    	$sql="SELECT * FROM tp_price where id=$id limit 1";
+    	return $db->fetchRow($sql);
+    }
+    function getAllPriceDetailById($id){
+    	$db = $this->getAdapter();
+    	$sql="SELECT * FROM tp_price_detail where price_id=$id ";
+    	return $db->fetchAll($sql);
+    }
+    
+    function getAllSupplyer(){
+    	$db = $this->getAdapter();
+    	$sql="SELECT id,supplyerName AS name FROM tp_supplier where supplyerName!='' and status=1 ";
+    	return $db->fetchAll($sql);
+    }
+    
+	function getAllLocation(){
+		$db = $this->getAdapter();
+		$sql="SELECT id,locationName AS name FROM tp_location where locationName!='' and status=1 ";
+		return $db->fetchAll($sql);
+	}
+	
+	function getAllVehicleType(){
+		$db = $this->getAdapter();
+		$sql="SELECT id,title AS name FROM tp_vehicletype where title!='' and status=1 ";
+		return $db->fetchAll($sql);
+	}
+}
+
